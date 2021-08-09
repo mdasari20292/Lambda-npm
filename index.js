710af4da36ea8a041b41d1e0ac8a452c5cba5c87
Mahendar Dasari <mahendar.dasari@factset.com>
2021-08-09 12:07:29 +0530

    added cli

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const {v4 : uuidv4 } =require('uuid');
 uuidv4();
 exports.handler = async (event) => {
     const uuid = uuidv4();;
-    return 'uuid: '+uuid;
+    return 'Update wih cli: '+uuid;
     
 };
